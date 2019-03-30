b12e28c6ca85ade1ce24f90bc21bedb930c2db14
ChenJX97 <36843169+ChenJX97@users.noreply.github.com>
2019-03-30 19:32:38 +0800

    init project

---
+++ src/main/java/com/xer97/mymall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.xer97.mymall.dao;
+
+import com.xer97.mymall.pojo.Category;
+
+public interface CategoryMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Category record);
+
+    int insertSelective(Category record);
+
+    Category selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Category record);
+
+    int updateByPrimaryKey(Category record);
+}
---
+++ src/main/java/com/xer97/mymall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.xer97.mymall.dao;
+
+import com.xer97.mymall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
